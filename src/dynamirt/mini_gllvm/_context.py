9f85b554aec277a268775d158db99a83aa95a540
Jonas K <kupschusJ@gmail.com>
2026-08-14 14:53:01 +0200

    Made _factorize and _design methods on _Context

--- src/dynamirt/mini_gllvm/_context.py
+++ src/dynamirt/mini_gllvm/_context.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from typing import Mapping
 
 from jax.typing import ArrayLike
+import numpy as np
 
 # minimal context dataclass passed to subfunctions
 @dataclass(frozen=True, eq=False)
@@ -12,3 +13,17 @@ class _Context:
     n_obs: int
     n_var: int
     n_latent: int
+    
+    def _factorize(self, key):
+        """interpret covariate as index column for grouping"""
+        if key is None:
+            return np.zeros(self.n_obs, int), 1
+        codes, idx = np.unique(np.asarray(self.covariates[key]).ravel(), return_inverse=True)
+        return idx, codes.size
+
+    def _design(self, predictors):
+        """stack multiple covariates into single array if multiple were given. 
+        Broadcast scalar values to correct length."""
+        keys = [predictors] if isinstance(predictors, str) else list(predictors)
+        cols = [np.broadcast_to(np.asarray(self.covariates[k]).ravel(), (self.n_obs,)) for k in keys]
+        return np.stack(cols, axis=-1)
